6c13c79376be5b0abc8290a5217f87bdf5eb6907
guillaume <guillaume.blanc.etudiant@gmail.com>
2016-04-19 13:38:04 +0200

    suppression de cakephp, trop peu de temps pour maitriser ce framework ...

--- Mouly - OCI-Oracle/OCI-Oracle/Bdd.sql
+++
@@ -1,7 +0,0 @@
-create table personnes (id number(2) primary key, nom char(20));
-
-insert into personnes values (0, 'xav0');
-insert into personnes values (1, 'xav1');
-insert into personnes values (2, 'xav2');
-insert into personnes values (3, 'xav3');
-insert into personnes values (4, 'xav4');
