799a59d4c9d7ef7be3b7ec5b598bfa2458a66862
guillaume <guillaume.blanc.etudiant@gmail.com>
2016-04-30 13:44:13 +0200

    index elise

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Auto-école</title>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+        <link href="dist/css/timeline.css" rel="stylesheet">
+        <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+        <link href="bower_components/morrisjs/morris.css" rel="stylesheet">
+        <link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <header>
+            <!-- Navigation -->
+            <?php include('nav.php');?>
+        </header>
+        
+    </body>
+</html>
