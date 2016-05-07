dc70718b830b6e7646e3bf22cbd755759375728e
guillaume <guillaume.blanc.etudiant@gmail.com>
2016-05-07 17:30:31 +0200

    test

--- model/provider/FormuleProvider.php
+++ model/provider/FormuleProvider.php
@@ -48,7 +48,7 @@ class FormuleProvider {
         while ($resultat = oci_fetch_array($req)) {
             foreach ($resultat as $formule) {
                 echo "cration des objets formules";
-                echo $formule['id_formule'];
+                echo $formule;
                 $formules = new Formule($formule['id_formule'], 
                                           $formule['prix_formule'], 
                                           $formule['nb_tickets_formule'],
